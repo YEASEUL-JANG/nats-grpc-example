30f5f2acda5528edbe6ec54ab8d8afe7b52e2e94
YEASEUL JANG <changys1212@naver.com>
2024-08-07 09:52:55 +0900

    grcp 초기설정

---
+++ src/main/java/com/example/nats_grpc_example/NatsGrpcExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.nats_grpc_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatsGrpcExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatsGrpcExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nats_grpc_example/NatsGrpcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nats_grpc_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatsGrpcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
